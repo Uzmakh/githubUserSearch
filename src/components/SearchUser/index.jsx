3e78d4f661d9df1be55d933919ade72e127487c6
uzma-khan98 <uzma.uk.1998@gmail.com>
2025-11-05 15:04:22 +0500

    Updated files

--- src/components/SearchUser/index.jsx
+++ src/components/SearchUser/index.jsx
@@ -14,17 +14,17 @@ const SearchUser = () => {
 
 
   return (
-    <div className="w-full h-[50px] flex items-center shadow-sm px-4 sm:h-[70px] rounded-lg search-bar">
+    <div className="w-full h-[50px] flex items-center shadow-lg px-4 mb-8 sm:h-[70px] rounded-lg search-bar">
       <img
         src={SearchIcon}
         alt=""
-        className="inline-block mr-2 w-5 sm:w-8"
+        className="inline-block mr-1 w-5 sm:w-8 sm:mr-4"
       />
       <input
         type="text"
         placeholder="Enter GitHub username..."
         value={state.username}
-        className="w-full text-[0.8rem] sm:text-2xl focus:outline-0"
+        className="w-full text-[1rem] tracking-tight sm:text-2xl focus:outline-0"
         onChange={handleOnChange}
       />
       {state.error && <p className="text-red-500 w-48 mr-2">{state.error}</p>}
--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -14,73 +14,109 @@ const UserInfo = () => {
 
   if (!user) {
     return (
-      <div className="mt-4 p-4 rounded-md shadow-sm user-info">
-        <p className="text-red-500">No User information available!</p>
+      <div className="mt-4 py-8 px-8 sm:px-16 sm:text-center leading-8 rounded-md shadow-xl user-info">
+        <h3 className="pb-4 font-bold text-xl text-center">
+          No results found!
+        </h3>
+
+        <p className="text-left md:text-center">
+          {" "}
+          We couldn't find any Github users matching your search. Please
+          double-check the username and try again.
+        </p>
       </div>
     );
   }
 
   return (
-    <div className="mt-8 rounded shadow-sm mb-12">
+    <div className="mt-8 rounded shadow-lg mb-12 ">
       <div className="user-info h-auto pt-8 px-4 flex flex-col">
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-6 md:flex md:flex-row">
           <div className="md:flex-1">
             <img
               src={user.avatar_url}
               alt={`${user.login}'s avatar`}
-              className="w-24 h-24 rounded-full"
+              className="w-24 h-24 rounded-full md:w-full md:h-auto md:rounded-full"
             />
           </div>
 
           <div className="md:flex-6">
-            <div className="flex justify-between flex-col">
-              {user.name && <h2>{user.name}</h2>}
+            <div className="flex justify-around flex-col md:grid md:grid-cols-2 md:gap-6">
               <div>
+                {user.name && <h2>{user.name}</h2>}
+
                 <a href={user.html_url} target="_blank" rel="no referrer">
                   @{user.login}
                 </a>
               </div>
-              <p>Joined:{new Date(user.created_at).toLocaleDateString()}</p>
+              <div>
+                <p>Joined:{new Date(user.created_at).toLocaleDateString()}</p>
+              </div>
             </div>
           </div>
         </div>
         <div className="my-8 leading-8">
-          <p>{user.bio}</p>
+          {user.bio ? <p>{user.bio}</p> : <p>This profile has no bio</p>}
         </div>
 
-        <div className="flex  justify-evenly px-6 py-4 rounded-lg w-full shadow-sm text-center leading-10 space-x-4 stats">
-          <p className="">
-            <strong> Repos</strong>
+        <div className="flex flex-col items-start mx-2 px-6 py-4 rounded-lg  shadow-sm  leading-10 space-x-4 sm:flex-row sm:justify-around  stats">
+          <p>
+            Repos
             <br />
-            {user.public_repos}
+            <strong>{user.public_repos}</strong>
           </p>
           <p>
-            <strong>Followers</strong>
+            Followers
             <br />
-            {user.followers}
+            <strong>{user.followers}</strong>
           </p>
           <p>
-            <strong>Following</strong>
+            Following
             <br />
-            {user.following}
+            <strong> {user.following}</strong>
           </p>
         </div>
 
-        <div className="more-info my-6 flex flex-col leading-10 space-y-2 ">
-          <div className="flex items-center">
-            <img src={LocationIcon} alt="" className="inline-block mr-4 bottom-icon"/>
-            {user.location}
+        <div className="more-info my-6 mx-2 flex flex-col leading-10 space-y-2 sm:flex-row sm:flex-wrap sm:justify-between sm:px-4 sm:items-center md:flex md:flex-row md:justify-between md:items-center  lg:flex lg:flex-row lg:justify-between lg:items-center">
+          <div>
+            <div className="flex items-center">
+              <img
+                src={LocationIcon}
+                alt=""
+                className="inline-block mr-4 bottom-icon"
+              />
+              {user.location ? <p>{user.location} </p> : <p> Not available</p>}
+            </div>
+            <div className="flex items-center">
+              <img
+                src={WebsiteIcon}
+                alt=""
+                className="inline-block mr-4 bottom-icon"
+              />
+              {user.blog ? <p>{user.blog} </p> : <p> Not available</p>}
+            </div>
           </div>
-          <div className="flex items-center">
-            <img src={WebsiteIcon} alt="" className="inline-block mr-4 bottom-icon"/>
-            {user.blog}
-          </div>
-          <div className="flex items-center">
-            <img src={TwitterIcon} alt=""className="inline-block mr-4 bottom-icon" />
-            {user.twitter_username}
-          </div>
-          <div className="flex items-center">
-            <img src={CompanyIcon} alt="" className="inline-block mr-4 bottom-icon"/>@{user.login}
+          <div>
+            <div className="flex items-center">
+              <img
+                src={TwitterIcon}
+                alt=""
+                className="inline-block mr-4 bottom-icon"
+              />
+              {user.twitter_username ? (
+                <p>{user.twitter_username} </p>
+              ) : (
+                <p> Not available</p>
+              )}
+            </div>
+            <div className="flex items-center">
+              <img
+                src={CompanyIcon}
+                alt=""
+                className="inline-block mr-4 bottom-icon"
+              />
+              @{user.login ? <p>{user.login} </p> : <p> Not available</p>}
+            </div>
           </div>
         </div>
       </div>
